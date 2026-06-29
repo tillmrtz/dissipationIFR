2e86796bdaa2cecd87aeee08e1b0d1d97d137a20
tillmrtz <t.moritz2000@gmail.com>
2026-06-29 11:53:02 +0200

    Added also the __init__.py files

---
+++ dissipationIFR/__init__.py
@@ -0,0 +1,8 @@
+# dissipationIFR/__init__.py
+
+__version__ = "0.0.0"
+__author__ = "Till Moritz"
+
+# Expose the main modules at the top level so users can do:
+# import dissipationIFR; dissipationIFR.plots.some_function()
+from dissipationIFR import plots, tools, utilities, interactive
