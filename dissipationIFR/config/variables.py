091035cdcc77dae5491274d7769468f7d3d7c909
tillmrtz <t.moritz2000@gmail.com>
2026-07-15 11:39:03 +0200

    Included adaptive and constant filter

--- dissipationIFR/config/variables.py
+++ dissipationIFR/config/variables.py
@@ -32,6 +32,24 @@ variables = {
         },
         'colormap': cmo.speed,
     },
+    'W_WATER': {'attributes': {
+        'long_name': 'Vertical Velocity of Water',
+        'units': 'cm/s',
+        },
+        'colormap': cmo.speed,
+    },
+    'N': {'attributes': {
+        'long_name': 'Brunt-Väisälä Frequency',
+        'units': '1/s',
+        },
+        'colormap': cmo.thermal,
+    },
+    'N_SORTED': {'attributes': {
+        'long_name': 'Sorted Brunt-Väisälä Frequency',
+        'units': '1/s',
+        },
+        'colormap': cmo.thermal,
+    },
     'SIGMA_T': {'attributes': {
         'long_name': 'Potential Density Anomaly',
         'units': 'kg/m³',
